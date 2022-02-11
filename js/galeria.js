d64de1e8e648cc6fef8aeb0c7dae4164d261e1fe
FeGaWeb <andresgalviscamacho21@gmail.com>
2022-02-11 00:13:02 -0500

    se agregó un alert y se pasaron medidas de px a % S sobre mi

---
+++ js/galeria.js
@@ -0,0 +1,18 @@
+
+/* function hover_imagenes(){
+
+    document.getElementById("imagen_1").onclick=opacidad_imagenes;
+
+}
+
+function opacidad_imagenes(){
+
+    var x=document.getElementById("imagen_1").style;
+    x.backgroundImage="none";
+} */
+
+function offcanvas_img(){
+
+    swal('Clic en la X para continuar');
+}
+
