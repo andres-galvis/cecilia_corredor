c5a788fff93508856c9b12f31f3954599f68592f
FeGaWeb <andresgalviscamacho21@gmail.com>
2022-04-21 22:43:35 -0500

    se automatizaron las cards de galeria

---
+++ js/cards.js
@@ -0,0 +1,281 @@
+let productosGenerales = [
+
+    {
+        id: "img--1",
+        nombre: "cuadro1",
+        img: '../img/img_galeria/1.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+        precio: "vendido",
+    },
+
+    {
+        id: "img--2",
+        nombre: "cuadro2",
+        img: '../img/img_galeria/2.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--3",
+        nombre: "cuadro3",
+        img: '../img/img_galeria/3.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--4",
+        nombre: "cuadro4",
+        img: '../img/img_galeria/4.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--5",
+        nombre: "cuadro5",
+        img: '../img/img_galeria/5.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--6",
+        nombre: "cuadro6",
+        img: '../img/img_galeria/6.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--7",
+        nombre: "cuadro7",
+        img: '../img/img_galeria/7.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--8",
+        nombre: "cuadro8",
+        img: '../img/img_galeria/8.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--9",
+        nombre: "cuadro9",
+        img: '../img/img_galeria/9.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--10",
+        nombre: "cuadro10",
+        img: '../img/img_galeria/10.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--11",
+        nombre: "cuadro11",
+        img: '../img/img_galeria/11.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--12",
+        nombre: "cuadro12",
+        img: '../img/img_galeria/12.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--13",
+        nombre: "cuadro13",
+        img: '../img/img_galeria/13.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--14",
+        nombre: "cuadro14",
+        img: '../img/img_galeria/14.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--15",
+        nombre: "cuadro15",
+        img: '../img/img_galeria/15.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--16",
+        nombre: "cuadro16",
+        img: '../img/img_galeria/16.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--17",
+        nombre: "cuadro17",
+        img: '../img/img_galeria/17.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--18",
+        nombre: "cuadro18",
+        img: '../img/img_galeria/18.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--19",
+        nombre: "cuadro19",
+        img: '../img/img_galeria/19.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--20",
+        nombre: "cuadro20",
+        img: '../img/img_galeria/20.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--21",
+        nombre: "cuadro21",
+        img: '../img/img_galeria/21.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--22",
+        nombre: "cuadro22",
+        img: '../img/img_galeria/22.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--23",
+        nombre: "cuadro23",
+        img: '../img/img_galeria/23.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--24",
+        nombre: "cuadro24",
+        img: '../img/img_galeria/24.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--25",
+        nombre: "cuadro25",
+        img: '../img/img_galeria/25.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--26",
+        nombre: "cuadro26",
+        img: '../img/img_galeria/26.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--27",
+        nombre: "cuadro27",
+        img: '../img/img_galeria/27.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--28",
+        nombre: "cuadro28",
+        img: '../img/img_galeria/28.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--29",
+        nombre: "cuadro29",
+        img: '../img/img_galeria/29.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--30",
+        nombre: "cuadro30",
+        img: '../img/img_galeria/30.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--31",
+        nombre: "cuadro31",
+        img: '../img/img_galeria/31.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+
+    {
+        id: "img--32",
+        nombre: "cuadro32",
+        img: '../img/img_galeria/32.jpg',
+        medidas: "120x210",
+        tecnica: "Oleo sobre lienzo",
+    },
+    
+]
+
+function renderProductos(arrayProductos) {
+    
+    const cardsContenedora = document.getElementById("cards_general")
+    cardsContenedora.innerHTML = ''
+    
+    arrayProductos.forEach(elementoDelArray => {
+        cardsContenedora.innerHTML += ` 
+        <div class="modal_container">
+            <img src="${elementoDelArray.img}" alt="" id="${elementoDelArray.id}">
+            <h3>Nombre:${elementoDelArray.nombre}</h3>
+            <h3>Tamaño:${elementoDelArray.medidas}</h3>
+            <h3>Técnica:${elementoDelArray.tecnica}</h3>
+            <h3>Precio: ${elementoDelArray.precio}</h3>
+        </div>`;
+    });
+}
+
+renderProductos(productosGenerales)
+
+
--- js/ligboximg.js
+++ js/ligboximg.js
@@ -1,8 +1,6 @@
 
 const btnCierra = document.querySelector('#btn-cierra')
-const btnAdelanta = document.querySelector('#btn-adelanta')
-const btnRetrocede = document.querySelector('#btn-retrocede')
-const imagenes = document.querySelectorAll('#galeria img')
+const imagenes = document.querySelectorAll('#cards_general img')
 const ligbox = document.querySelector('#contenedor--principal')
 
 const imagenActiva = document.querySelector('#img-activa')
@@ -25,28 +23,3 @@ imagenes.forEach((imagen) =>{
 btnCierra.addEventListener('click', () =>{
     ligbox.style.display = 'none';
 });
-
-//adelantar imagen
-
-const adelantaImagen = () =>{
-    if(indiceImg === imagenes.length -1){
-        indiceImg = -1
-    }
-    imagenActiva.src = imagenes [indiceImg + 1].src;
-    indiceImg++
-}
-
-btnAdelanta.addEventListener('click', adelantaImagen)
-
-//retroceder imagen
-
-const retrocedeImagen = () => {
-
-    if(indiceImg === 0){
-        indiceImg = imagenes.length;
-    }
-    imagenActiva.src = imagenes[indiceImg -1].src;
-    indiceimagen --;
-}
-
-btnRetrocede.addEventListener('click', retrocedeImagen)
